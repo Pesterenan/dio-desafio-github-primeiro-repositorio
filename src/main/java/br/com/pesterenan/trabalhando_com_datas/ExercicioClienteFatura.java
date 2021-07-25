b86f4d53075548dfc1071496a86982b7585d0d53
Renan Torres <pesterenan@gmail.com>
2021-07-25 20:34:15 -0300

    Exercício de Cálculo de Datas
    Exercício final do módulo "Trabalhando com Datas" do curso de Java
    básico da DigitalInnovation One

--- src/main/java/br/com/pesterenan/trabalhando_com_datas/ExercicioClienteFatura.java
+++ src/main/java/br/com/pesterenan/trabalhando_com_datas/ExercicioClienteFatura.java
@@ -5,8 +5,13 @@ import java.util.Calendar;
 public class ExercicioClienteFatura {
 
 	public static void main(String[] args) {
-		
-		Fatura fatura = new Fatura(Calendar.getInstance(), 2000);
+		Calendar dataVencimento = Calendar.getInstance();
+		// subtrair quatro dias na data de vencimento
+		dataVencimento.add(Calendar.DATE, -4);
+		// gerar fatura com a data de vencimento futura
+		Fatura fatura = new Fatura(dataVencimento, 2000);
+		System.out.println(fatura);
+		// pagar a fatura na data de hoje
 		fatura.pagarFatura(Calendar.getInstance());
 	}
 
--- src/main/java/br/com/pesterenan/trabalhando_com_datas/Fatura.java
+++ src/main/java/br/com/pesterenan/trabalhando_com_datas/Fatura.java
@@ -6,11 +6,11 @@ import java.util.Date;
 public class Fatura {
 	private double valorFatura;
 	private Calendar dataVencimento;
-	private int diasParaPagar = 10;
-	
-	
+	private int diasExtrasParaPagar = 10;
+
 	public Fatura(Calendar vencimento, double valor) {
 		this.dataVencimento = vencimento;
+		this.setValor(valor);
 	}
 
 	public Calendar getDataVencimento() {
@@ -24,14 +24,34 @@ public class Fatura {
 	public void setValor(double valor) {
 		this.valorFatura = valor;
 	}
-	
+
 	public void pagarFatura(Calendar diaPagamento) {
-		Calendar dataVencimentoExtra = Calendar.getInstance();
-		dataVencimentoExtra = dataVencimento;
-		dataVencimentoExtra.add(Calendar.DAY_OF_MONTH, diasParaPagar);
-		if (diaPagamento.before(dataVencimentoExtra)){
-			System.out.println(diaPagamento.getTime().toString());
-			System.out.println(dataVencimentoExtra.getTime().toString());
+		// clonar data de vencimento para não modificá-la
+		Calendar dataVencimentoExtra = (Calendar) this.getDataVencimento().clone();
+		// adicionar dias extras para pagar
+		dataVencimentoExtra.add(Calendar.DATE, diasExtrasParaPagar);
+		// checar se a nova data cai num sábado ou domingo e adicionar dias 
+		// para cair na próxima segunda
+		switch (dataVencimentoExtra.get(Calendar.DAY_OF_WEEK)) {
+		case Calendar.SATURDAY:
+			dataVencimentoExtra.add(Calendar.DATE, 2);
+			break;
+		case Calendar.SUNDAY:
+			dataVencimentoExtra.add(Calendar.DATE, 1);
+			break;
+		}
+		// verificar se a fatura já venceu:
+		if (this.getDataVencimento().before(diaPagamento)) {
+			System.out.printf("Sua fatura está vencida. Você tem até o dia \"%tc\" para pagar.\n\n", dataVencimentoExtra.getTime());
+			System.out.printf("Você está pagando no dia:  %tc\n", diaPagamento.getTime());
+			System.out.printf("Data vencimento original:  %tc\n", this.getDataVencimento().getTime());
+			System.out.printf("Você pode pagar até o dia: %tc\n", dataVencimentoExtra.getTime());
 		}
 	}
+
+	public String toString() {
+		return String.format("Essa fatura vence no dia \"%tc\", e tem o valor de R$%.2f.",
+				this.dataVencimento.getTime(), this.getValor());
+	}
+
 }
